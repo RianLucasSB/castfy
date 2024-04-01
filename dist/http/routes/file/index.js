70d832366fbe9e74c303a1116d527fbab7dfb1d2
Rian Lucas <rianlucas2014@gmail.com>
2024-04-01 15:34:08 -0300

    feat: update tsconfig.json and package.json

--- dist/http/routes/file/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fileHandler = void 0;
-const download_1 = require("./download");
-async function fileHandler(app) {
-    app.post('/', async () => { });
-    app.get('/:fileId', download_1.handleDownloadFile);
-}
-exports.fileHandler = fileHandler;
