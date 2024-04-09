c2a8274e0c0bb485f8efb05698222fbad340efe7
Rian Lucas <rianlucas2014@gmail.com>
2024-04-09 16:57:22 -0300

    feat: add cors

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,17 @@
 import fastify from 'fastify'
 import multipart from '@fastify/multipart'
+import cors from '@fastify/cors'
 import { authHandler } from './routes/auth'
 import { fileHandler } from './routes/file'
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 8080
 
 const server = fastify()
+
+server.register(cors, {
+  origin: '*',
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+})
 
 server.register(multipart, {
   limits: {
